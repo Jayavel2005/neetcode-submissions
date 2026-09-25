f4cbe7871e275699b2a0b8a56f133d34f4f248b1
L G JAYAVEL <147837290+Jayavel2005@users.noreply.github.com>
2026-09-25 13:34:33 +0530

    Add: rearrange-array-elements-by-sign - submission-0

---
+++ Data Structures & Algorithms/rearrange-array-elements-by-sign/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int i = 0; int j = 1;
+        int[] res = new int[nums.length];
+
+        for (int num : nums) {
+            if (num > 0) {
+                res[i] = num;
+                i+=2;
+            }
+            else if (num < 0 ){
+                res[j] = num;
+                j+=2;
+            }
+        }
+
+        return res; 
+    }
+}
